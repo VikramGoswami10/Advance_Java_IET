4200c058b05d7f7493996201cdc2c6e7aeb6cb66
vikram Goswami <vikramgoswami4k@gmail.com>
2025-01-06 09:52:05 +0530

    Day-17

---
+++ Day_17/SpringBootHelloMVC/src/main/java/com/demo/SpringBootHelloMVC/SpringBootHelloMvcApplication.java
@@ -0,0 +1,13 @@
+package com.demo.SpringBootHelloMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloMvcApplication.class, args);
+	}
+
+}
